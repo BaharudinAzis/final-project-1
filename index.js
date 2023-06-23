942abed076a147baf563d4283d633d95fd003d66
bahar <baharudinazis98@gmail.com>
2023-06-23 08:44:22 +0700

    update

--- index.js
+++ index.js
@@ -17,3 +17,49 @@ function disableDarkMode() {
   document.body.classList.remove('dark-mode');
   // Hapus preferensi dark mode pengguna di sini (misalnya, dari local storage)
 }
+  // Membuat component sticky menggunakan JavaScript
+  var stickyComponent = document.createElement('div');
+  stickyComponent.className = 'sticky';
+
+  var closeButton = document.createElement('button');
+  closeButton.textContent = 'Close';
+  stickyComponent.appendChild(closeButton);
+
+  var downloadText = document.createElement('p');
+  downloadText.textContent = 'Download Aplikasi Kami';
+  stickyComponent.appendChild(downloadText);
+
+  var googlePlayButton = document.createElement('button');
+  googlePlayButton.textContent = 'Google Play Store';
+  stickyComponent.appendChild(googlePlayButton);
+
+  var appStoreButton = document.createElement('button');
+  appStoreButton.textContent = 'Apple App Store';
+  stickyComponent.appendChild(appStoreButton);
+
+  // Cek apakah sudah pernah close sebelumnya
+  var isClosed = localStorage.getItem('stickyClosed');
+
+  if (isClosed === null) {
+      // Jika belum pernah close, tambahkan component sticky ke body
+      document.body.appendChild(stickyComponent);
+  }
+
+  // Tambahkan event listener untuk tombol close
+  closeButton.addEventListener('click', function () {
+      // Tandai component sudah ditutup
+      localStorage.setItem('stickyClosed', 'true');
+
+      // Hapus component dari body
+      document.body.removeChild(stickyComponent);
+  });
+
+  // Redirect ke Google Play Store
+  googlePlayButton.addEventListener('click', function () {
+      window.location.href = 'https://play.google.com/';
+  });
+
+  // Redirect ke Apple App Store
+  appStoreButton.addEventListener('click', function () {
+      window.location.href = 'https://www.apple.com/id/app-store/';
+  });
